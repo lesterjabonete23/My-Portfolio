7c8292036bd26ad9546edc5502263e411557509b
Lester <88809118+lesterjabonete23@users.noreply.github.com>
2022-08-08 22:43:09 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -100,26 +100,26 @@
     })), e(window).on("load", (function() { e(".preloader-icon").fadeOut(400), e(".preloader").delay(500).fadeOut("slow") }))
 }(jQuery);
 
-// document.addEventListener('contextmenu', function(e) {
-//     e.preventDefault();
-// });
-// document.onkeydown = function(e) {
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (event.keyCode == 123) {
-//         return false;
-//     }
-// }
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+    if (event.keyCode == 123) {
+        return false;
+    }
+}
